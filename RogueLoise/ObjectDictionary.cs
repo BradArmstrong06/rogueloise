335572e7181a7c6b475a044aea9a738b424276d1
Aseev German <Aseev German@localhost>
2014-02-11 17:52:58 +0400

    ObjectDictionary

---
+++ RogueLoise/ObjectDictionary.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace RogueLoise
+{
+    public class ObjectDictionary
+    {
+        private Dictionary<string, GameObject> _objects = new Dictionary<string, GameObject>();
+
+        public void Add(GameObject obj)
+        {
+            _objects.Add(obj.Key, obj); //todo exc catch
+        }
+
+        public GameObject this[string key]
+        {
+            get
+            {
+                return Get(key);
+            }
+        }
+
+        public GameObject Get(string key)
+        {
+            GameObject obj;
+            if (_objects.TryGetValue(key, out obj))
+            {
+                return obj.Clone();
+            }
+            return null;
+        }
+    }
+}
